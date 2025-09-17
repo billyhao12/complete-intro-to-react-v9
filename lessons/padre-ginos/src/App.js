15320dd5e6bc562ad3252b12c45435dffb735bdb
Billy Hao <wshao@sbgtv.com>
2025-09-17 11:38:48 -0700

    set up prettier

--- lessons/padre-ginos/src/App.js
+++ lessons/padre-ginos/src/App.js
@@ -6,33 +6,29 @@ const Pizza = (props) => {
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Padre Gino's Pizza"),
-      React.createElement(Pizza, {
-        name: "The Pepperoni Pizza",
-        description: "some dope pizza yo"
-      }),
-      React.createElement(Pizza, {
-        name: "Americano Pizza",
-        description: "French fries and hot dogs"
-      }),
-      React.createElement(Pizza, {
-        name: "The Hawaiian",
-        description: "pineapple and ham"
-      }),
-      React.createElement(Pizza, {
-        name: "Chicken Pizza",
-        description: "chicken nuggets on your pizza"
-      }),
-      React.createElement(Pizza, {
-        name: "Baked Potato Pizza",
-        description: "potato mash"
-      }),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's Pizza"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "some dope pizza yo",
+    }),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "French fries and hot dogs",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian",
+      description: "pineapple and ham",
+    }),
+    React.createElement(Pizza, {
+      name: "Chicken Pizza",
+      description: "chicken nuggets on your pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "Baked Potato Pizza",
+      description: "potato mash",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
